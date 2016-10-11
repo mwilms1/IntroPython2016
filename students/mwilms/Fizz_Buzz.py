e59ee03ade0c432787e18a2e9fe5b621a5982e5b
Marcus <marcuspolo77@Marcuss-MacBook-Pro.local>
2016-10-11 05:04:01 -0700

    Fibonacci

---
+++ students/mwilms/Fizz_Buzz.py
@@ -0,0 +1,9 @@
+for item in range(1, 101):
+...     if item % 15 == 0:
+...         print(FizzBuzz)
+...     elif item % 5 == 0:
+...         print(Buzz)
+...     elif item % 3 == 0:
+...         print(Fizz)
+...     else:
+...         print(item)
